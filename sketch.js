316afde695c0b7598b3bfc848a5a47ea005ad149
gilbertjacke <gilbertjacke@gmail.com>
2017-01-05 14:55:22 +0000

    .gitignore

---
+++ sketch.js
@@ -0,0 +1,19 @@
+let blob;
+
+let blobs = [];
+
+function setup(){
+  createCanvas(600,400)
+  blob = new Blob(width/2, height/64);
+  for (var i = 0; i < 10; i++){
+    blobs[i] = new Blob(16);
+  }
+}
+
+function draw(){
+  background(0);
+  blob.show()
+  for (var i = 0; i < blobs.lenght; i ++){
+    blobs[i].show();
+  }
+}
