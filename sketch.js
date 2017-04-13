5d9876bdece43232dca20c1a589fc31e115f27c0
gilbertjacke <gilbertjacke@gmail.com>
2017-04-13 16:46:54 +0100

    multiple blobs

--- sketch.js
+++ sketch.js
@@ -3,11 +3,17 @@ var blob;
 var blobs = [];
 
 function setup() {
-  createCanvas(600,400)
-  blob = new Blob();
+  createCanvas(600,600)
+  blob = new Blob(width/2, height/2, 64);
+  for (var i = 0; i < 10; i++){
+    blobs[i] = new Blob(random(width), random(height), 16);
+  }
 }
 
 function draw() {
   background(0);
-  blob.show()
+  blob.show();
+  for (var i = 0; i < blobs.length; i++) {
+    blobs[i].show();
+  }
 }
